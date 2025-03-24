79b88f22855a98a21e75c99466d277c9ef59cc2e
jojun05 <cae616788@gmail.com>
2025-03-24 18:12:20 +0900

    week03

--- week03.py
+++ week03.py
@@ -1,10 +1,11 @@
 def inters(l1, l2):
-    l3 = []
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+    s1 = set(l1)
+    s2 = set(l2)
+
+    # return list(s1.intersection(s2))
+    return list(s1 - s2)
 
 
 l1 = [2, 32, 1, 44, 55, 66, 77]
 l2 = [23, 2, 6, 32, 1, 55, 66, 13, 19, 29]
+print(inters(l1, l2))
