d252e67ada068852c751b5c6947ffb0916e916a2
jojun05 <cae616788@gmail.com>
2025-04-07 17:50:07 +0900

    v.04

--- week05.py
+++ week05.py
@@ -1,41 +1,11 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty!"
-        popped_node = self.top
-        self.top = self.top.link
-        popped_node.link = None
-        return popped_node.data
-
-    def peek(self): #peek이 탑을 가르키고 있어서 top.data임
-        return self.top.data
-
-
-s1 = Stack()
-#print(s1.pop())
-s1.push("Data structure")
-s1.push("Database")
-#print(s1.pop())
-s1.pop() #<- 이건 database를 없앤것
-print(s1.peek())
-#for i in range(2):
-#print(s1.pop())
-
+s1 = []
+s1.append("Data structure") #push
+s1.append("Database")
+print(s1[-1]) #peek처럼 동작하는얘
+print(s1)
+print(s1.pop()) #pop이라서 삭제
+print(s1)
+print(s1.pop()) #pop이라서 삭제
+print(s1)
+print(s1.pop()) #indexerr
+print(s1)
