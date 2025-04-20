5413ddd6d12d1a059163b9c906d872a72f1e1ad9
dyd <162683643+Jojun05@users.noreply.github.com>
2025-04-20 21:14:11 +0900

    Update week04.py

--- week04.py
+++ week04.py
@@ -2,23 +2,25 @@
 
 class Node:
     def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
+        self.data = data #이게 노트에 저장된 데이터임
+        self.link = link #이건 노드를 가르키는 링크
 
-
+#링크스 리스트이게 연결 리스트를 관리를 함
 class LinkedList:
     def __init__(self):
-        self.head = None
-
+        self.head = None #일단 연결 리스트의 첫번째 노드인 head를 초기화해 none으로 설정
+        
 
     def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.link:
-            current = current.link  # move current
-        current.link = Node(data)
+        #연결 리스트가 비어있다? 그러면 새로운 노드를 head로 설정
+        if not self.head: #
+            self.head = Node(data) #새로운 node객체를 생성하여 head에 저장
+            return #함수 종료
+            
+        current = self.head #시작점을 head로 설정을 하고
+        while current.link: #while None이 아니면 반복
+            current = current.link  # move current 다음 노드로 간다는 소리임
+        current.link = Node(data) #새로운 node 객체를 생성해
 
     def remove(self, target):
         current = self.head
