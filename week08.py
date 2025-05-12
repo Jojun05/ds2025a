9a34273da88e14171aacfb9dd2d570a230a97a08
jojun05 <cae616788@gmail.com>
2025-05-12 16:59:15 +0900

    v3.8 tree

--- week08.py
+++ week08.py
@@ -28,17 +28,15 @@ def search(find_number):
     current = root
     while True:
         if find_number == current.data:
-            print(f"{find_number}을(를) 찾았습니다")
-            break
+            return True
+
         elif find_number < current.data:
             if current.left is None:
-                print(f"{find_number}이(가) 존재하지 않습니다")
-                break
+                return False
             current = current.left
         else:
             if current.right is None:
-                print(f"{find_number}이(가) 존재하지 않습니다")
-                break
+                return False
             current = current.right
 
 
@@ -80,8 +78,13 @@ if __name__ == "__main__":
     print('BST 구성 완료')
     post_order(root)
     print()
-    find_number = int(input("찾는 수는?"))
-    search(find_number)
+    find_number = int(input("찾는 수는? "))
+    if search(find_number):
+        print(f"{find_number}을(를) 찾았습니다")  # return True
+    else:
+        print(f"{find_number}이(가) 존재하지 않습니다")  # return False
+
+
 
     # find_number = int(input())
     # current = root
