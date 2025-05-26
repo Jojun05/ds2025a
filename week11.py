4744e856e767dd8c17050d1ea031491161e7e61f
jojun05 <cae616788@gmail.com>
2025-05-26 16:35:54 +0900

    2.1

--- week11.py
+++ week11.py
@@ -9,19 +9,18 @@ graph = [
     [0, 0, 0, 0, 0, 0, 1, 0]
 ]
 
-
 def dfs(g, i, visited):
     visited[i] = True
-    print(chr(ord('A') + i), end=' ')
-
-    for j in range(len(g)):
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(graph)):
         if g[i][j] == 1 and not visited[j]:
             dfs(g, j, visited)
-def bfs(g,i,visited):
+
+def bfs(g, i, visited):
     pass
 
 visited_dfs = [False for _ in range(len(graph))]
 visited_bfs = [0 for _ in range(len(graph))]
 dfs(graph, 7, visited_dfs)
 print()
-bfs(graph,4,visited_bfs)
+bfs(graph, 4, visited_bfs)
